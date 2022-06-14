e44a00c819d204aba2cc56aaa2f82c86f6dbcc0d
Renan Oliveira <renanoliveirasjs@gmail.com>
2022-06-14 09:50:06 -0300

    Remove event.preventDefault()

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 function usuario_guardado() {
-    event.preventDefault();
-    let url = "http://127.0.0.1:5500/teste.html";
+    // event.preventDefault();
 
     
     let email = document.getElementById("email").value;
